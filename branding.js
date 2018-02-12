8521872d7188c17d325f23a0d7cf4c79d28feae9
jason <tingsong@pku.edu.cn>
2018-02-12 21:11:44 +0800

    clone

---
+++ branding.js
@@ -0,0 +1,7 @@
+function ucbheader(){
+	document.write('<img src="ucb.png" border="0" usemap="#UCBMap" /><map name="UCBMap" id="UCBMap"><area shape="rect" coords="11,3,298,27" href="http://www.colorado.edu/" alt="University of Colorado at Boulder" target="_blank" /><area shape="rect" coords="794,7,828,25" href="http://www.colorado.edu/" alt="CU Home" target="_blank" /><area shape="rect" coords="835,7,873,25" href="http://www.colorado.edu/search/" alt="CU Search" target="_blank"  /><area shape="rect" coords="880,7,916,25" href="http://www.colorado.edu/atoz/" alt="CU A to Z" target="_blank"  /><area shape="rect" coords="925,7,949,25" href="http://www.colorado.edu/campusmap/" alt="Campus Map" target="_blank" /></map>');
+}
+
+function ucbfooter(){
+	document.write('<a target="_blank" href="http://www.colorado.edu"><img src="wordmark.png" alt="University of Colorado at Boulder" /></a>');	
+}
